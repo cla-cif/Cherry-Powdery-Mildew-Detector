15b56f6b6d3e268cc94da5fe18c7068ff767b998
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2023-01-17 20:21:56 +0000

    create app.py file link to the streamlit pages to be created

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_leaves_visualizer import page_cells_visualizer_body
+from app_pages.page_powdery_mildew_detector import page_malaria_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
+app = MultiPage(app_name="Malaria Detector")  # Create an instance of the app
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
+app.add_page("Powdery Mildew detector", page_powdery_mildew_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run()  # Run the app
