a179658e696bfb2f12a4541c71d3e1e5c81b4d44
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2023-01-17 21:59:45 +0000

    delete output v3 basic code of data management.py

---
+++ src/data_management.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os
+import base64
+from datetime import datetime
+import joblib
+
+
+def download_dataframe_as_csv(df):
+
+    datetime_now = datetime.now().strftime("%d%b%Y_%Hh%Mmin%Ss")
+    csv = df.to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = (
+        f'<a href="data:file/csv;base64,{b64}" download="Report {datetime_now}.csv" '
+        f'target="_blank">Download Report</a>'
+    )
+    return href
+
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
