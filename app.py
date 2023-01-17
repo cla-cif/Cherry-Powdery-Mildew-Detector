736919135bfbad3d7fdcc3e8b195da63a2d1223b
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2023-01-17 22:24:07 +0000

    code multipage.py, change names of pages to load

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_leaves_visualizer import page_cells_visualizer_body
-from app_pages.page_powdery_mildew_detector import page_malaria_detector_body
+from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
