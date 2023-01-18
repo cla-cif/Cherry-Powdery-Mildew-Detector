22dd1f8d26c91f471d371a1ec836be01b75df729
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2023-01-18 20:41:36 +0000

    code page_project_hypotesis for streamlit dashboard

---
+++ app_pages/page_project_hypotesis.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* We suspect cherry leaves affected by powdery mildew have clear marks, "
+        f"typically the first symptom is a light-green, circular lesion on either leaf surface,"
+        f"then a subtle white cotton-like growth develops in the infected area. \n\n"
+        f"* An Image Montage shows the evident difference between a healthy leaf and an infected one. "
+        f"Average Image, Variability Image and Difference between Averages samples did not reveal "
+        f"any clear pattern to differentiate one from another."
+
+    )
