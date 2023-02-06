c6466ee0df3f4f13504f65990756917184663a76
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2023-02-06 23:29:20 +0000

    display cm, roc, loss graph on page_ml_performance and train new model£

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -10,20 +10,21 @@ def page_ml_performance_metrics():
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
-    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    labels_distribution = plt.imread(f"outputs/{version}/number_leaves_sets.png")
     st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
     st.write("---")
 
 
     st.write("### Model History")
-    col1, col2 = st.columns(2)
-    with col1: 
-        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_acc, caption='Model Training Accuracy')
-    with col2:
-        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
-        st.image(model_loss, caption='Model Training Losses')
-    st.write("---")
+
+    model_roc = plt.imread(f"outputs/{version}/roccurve.png")
+    st.image(model_roc, caption='ROC Curve')
+
+    model_cm = plt.imread(f"outputs/{version}/confusion_matrix.png")
+    st.image(model_cm, caption='Confusion Matrix')
+
+    model_perf = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_perf, caption='Model Performance')  
 
     st.write("### Generalised Performance on Test Set")
     st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
